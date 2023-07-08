e4facaa0d18c3d9137f83dc6bad84b2ac0dfb569
nelsonic <nelson+github@dwyl.com>
2023-07-08 12:45:37 +0100

    temporarily ignore API pipeline in router.ex as not used (yet!) #23

--- lib/atm_web/router.ex
+++ lib/atm_web/router.ex
@@ -10,9 +10,9 @@ defmodule AtmWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", AtmWeb do
     pipe_through :browser
